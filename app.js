ac3f7020d6dd842b837535bca79889a2667c9e53
Zachary Temkin <zachtemkin@gmail.com>
2023-05-29 17:46:02 -0400

    adds array of activities and subscribers

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 require("dotenv").config();
-// const AccessToken = require("twilio").jwt.AccessToken;
-// const BoxPermission = AccessToken.BoxPermission;
 const express = require("express");
 const app = express();
 const port = 3000;
@@ -14,23 +12,113 @@ const twilioClient = require("twilio")(
   { accountSid: process.env.TWILIO_ACCOUNT_SID }
 );
 
-const subscribers = [
-  { name: "Zach", number: "+17189083928" },
-  { name: "Dan", number: "+19144798900" },
+const activities = [
+  {
+    name: "Bar Hangout",
+    subscribers: [
+      { name: "Zach", number: "+17189083928" },
+      { name: "Dan", number: "+19144798900" },
+    ],
+  },
+  {
+    name: "Game night",
+    subscribers: [
+      { name: "Zach", number: "+17189083928" },
+      { name: "Dan", number: "+19144798900" },
+    ],
+  },
+  {
+    name: "Dancing",
+    subscribers: [
+      { name: "Zach", number: "+17189083928" },
+      { name: "Dan", number: "+19144798900" },
+    ],
+  },
+  {
+    name: "Fancy Dinner",
+    subscribers: [
+      { name: "Zach", number: "+17189083928" },
+      { name: "Dan", number: "+19144798900" },
+    ],
+  },
+  {
+    name: "Casual Dinner",
+    subscribers: [
+      { name: "Zach", number: "+17189083928" },
+      { name: "Dan", number: "+19144798900" },
+    ],
+  },
+  {
+    name: "Hiking",
+    subscribers: [
+      { name: "Zach", number: "+17189083928" },
+      { name: "Dan", number: "+19144798900" },
+    ],
+  },
+  {
+    name: "Walk to BK",
+    subscribers: [
+      { name: "Zach", number: "+17189083928" },
+      { name: "Dan", number: "+19144798900" },
+    ],
+  },
+  {
+    name: "Hang outside",
+    subscribers: [
+      { name: "Zach", number: "+17189083928" },
+      { name: "Dan", number: "+19144798900" },
+    ],
+  },
+  {
+    name: "Apartment hang",
+    subscribers: [
+      { name: "Zach", number: "+17189083928" },
+      { name: "Dan", number: "+19144798900" },
+    ],
+  },
+  {
+    name: "Themed Activity",
+    subscribers: [
+      { name: "Zach", number: "+17189083928" },
+      { name: "Dan", number: "+19144798900" },
+    ],
+  },
+  {
+    name: "Workout",
+    subscribers: [
+      { name: "Zach", number: "+17189083928" },
+      { name: "Dan", number: "+19144798900" },
+    ],
+  },
+  {
+    name: "I'm bored",
+    subscribers: [
+      { name: "Zach", number: "+17189083928" },
+      { name: "Dan", number: "+19144798900" },
+    ],
+  },
 ];
 
 app.get("/", (req, res) => {
   const activity = req.query.activity;
+  let currentActivity;
 
-  subscribers.forEach((sub) => {
-    twilioClient.messages.create({
-      body: `Hello ${sub.name}, You have been invited to participate in ${activity} with Raha & Rachel.`,
-      from: "+18339383052",
-      to: `${sub.number}`,
+  if (!activities.find((e) => e.name === activity)) {
+    console.log("invalid activity");
+  } else {
+    currentActivity =
+      activities[activities.findIndex((e) => e.name === activity)];
+
+    currentActivity.subscribers.forEach((sub) => {
+      twilioClient.messages.create({
+        body: `Hello ${sub.name}, You have been invited to participate in ${activity} with Raha & Rachel.`,
+        from: "+18339383052",
+        to: `${sub.number}`,
+      });
     });
-  });
+  }
 
-  res.send(activity);
+  res.send(currentActivity);
 });
 
 app.listen(port, () => {
